75d32c225d1fcdf0bfc1dbb289b98bd11b1919f5
Vadym Maslovskyi <vmaslovskyi@perpet.io>
2023-04-30 14:50:26 +0300

    base general configuration and connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeModule} from "@nestjs/sequelize";
+
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+          envFilePath: ".env"
+      }),
+      SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
+      }),
+      AuthModule,
+      UserModule,
+      RoleModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
