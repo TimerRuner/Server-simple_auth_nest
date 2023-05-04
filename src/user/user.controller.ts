375abe8df69e8e9e45a93a73b2e20f6d81b82642
Vadym Maslovskyi <vmaslovskyi@perpet.io>
2023-05-04 22:31:35 +0300

    create controller and service for user and role instances

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UserService } from "./user.service";
+import { UpdateRoleDto } from "./dto/update-role.dto";
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post("create")
+  async createUser(@Body() dto: CreateUserDto) {
+    return await this.userService.create(dto)
+  }
+
+  @Get()
+  async getAll(){
+    return this.userService.getAll()
+  }
+
+
+  @Patch("update/:id")
+  async updateUser(@Param("id") id: number, @Body() dto: CreateUserDto) {
+    return this.userService.updateUser(id, dto)
+  }
+
+  @Patch("add-role/:id")
+  async addUserRole(@Param("id") id: number, @Body() dto: UpdateRoleDto) {
+    return this.userService.addRole(id, dto)
+  }
+
+  @Delete("delete/:id")
+  async deleteUser(@Param("id") id: string) {
+    return this.userService.delete(id)
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Roles } from "../role/roles.model";
+import { Users } from "./user.model";
+import { UserRoles } from "../role/user-roles.model";
+import { RoleModule } from "../role/role.module";
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  imports: [
+    SequelizeModule.forFeature([Roles, Users, UserRoles]),
+    RoleModule
+  ],
 })
 export class UserModule {}
