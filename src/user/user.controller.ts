90c9bae1dfd8c9d5719e38c3d78e5f2eb9bba3db
Vadym Maslovskyi <vmaslovskyi@perpet.io>
2023-05-06 00:37:41 +0300

    realize role and jwt security guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UserService } from "./user.service";
 import { UpdateRoleDto } from "./dto/update-role.dto";
+import { AuthGuard } from "../auth/jwt-auth.guard";
+import { Roles } from "../auth/role.decorator";
+import { RolesGuard } from "../auth/role.guard";
 
 @Controller('user')
 export class UserController {
@@ -11,6 +14,7 @@ export class UserController {
     return await this.userService.create(dto)
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   async getAll(){
     return this.userService.getAll()
@@ -26,6 +30,8 @@ export class UserController {
     return this.userService.updateUser(id, dto)
   }
 
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
   @Patch("add-role/:id")
   async addUserRole(@Param("id") id: number, @Body() dto: UpdateRoleDto) {
     return this.userService.addRole(id, dto)
